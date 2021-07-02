e34f44a456d4439ad1d2bb128a10b8d9dd61fee3
rishabhnohwal <32811483+rishabhnohwal@users.noreply.github.com>
2021-07-02 21:35:12 +0530

    Update StringFormat.java

--- src/main/java/com/stackroute/streams/StringFormat.java
+++ src/main/java/com/stackroute/streams/StringFormat.java
@@ -11,11 +11,11 @@ public class StringFormat {
             return "Give proper input not empty list";
         }
 
-        if (input.stream().anyMatch(a->a<0)){
+        if (input.stream().anyMatch(num->num<0)){
             return "Give proper input not negative values";
         }
 
-       List<String> list = input.stream().map(a->a%2!=0?",odd"+a:",even"+a).collect(Collectors.toList());
+       List<String> list = input.stream().map(num->num%2!=0?",odd"+num:",even"+num).collect(Collectors.toList());
         String ans="";
         for (String ele:list){
             ans+=ele;
